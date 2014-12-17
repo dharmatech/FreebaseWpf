7cbb57edd758cf7b87e94e9e40054834e359f579
dharmatech <wayo.cavazos@gmail.com>
2014-12-16 21:41:51 -0600

    RequestTopic

--- Freebase WPF/MainWindow.xaml.cs
+++ Freebase WPF/MainWindow.xaml.cs
@@ -42,39 +42,43 @@ namespace Freebase_WPF
     {
         public MainWindow() { InitializeComponent(); }
 
+        private void RequestTopic(string topic, Action<IRestResponse> action)
+        {
+            label0.Content = "Loading";
+
+            var worker = new BackgroundWorker();
+
+            worker.DoWork += (sender, args) =>    
+                args.Result =
+                    new RestClient("https://www.googleapis.com")
+                        .Execute(new RestRequest(String.Format("freebase/v1/topic{0}", topic)));
+            
+            worker.RunWorkerCompleted += (sender, args) => 
+            {
+                var response = args.Result as IRestResponse;
+
+                label1.Content = response.StatusCode;
+
+                action(response);
+
+                label0.Content = "Ready";
+            };
+
+            worker.RunWorkerAsync();
+        }
+
         private void topicTextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                label0.Content = "Loading";
-
-                var backgroundWorker = new BackgroundWorker();
-
-                backgroundWorker.DoWork += (object s, DoWorkEventArgs arg) =>    
+                RequestTopic(topicTextBox.Text, response =>
                 {
-                    var topic = arg.Argument as string;
-
-                    arg.Result =
-                        new RestClient("https://www.googleapis.com")
-                            .Execute(new RestRequest(String.Format("freebase/v1/topic{0}", topic)));
-                };
-
-                backgroundWorker.RunWorkerCompleted += (object s, RunWorkerCompletedEventArgs arg) =>   
-                {
-                    var response = arg.Result as IRestResponse;
-
-                    label1.Content = response.StatusCode;
-
                     if (response.StatusCode == HttpStatusCode.OK)
                         topicPropertiesListBoxA.ItemsSource =
                             JsonConvert
                                 .DeserializeObject<FreebaseTopic>(response.Content)
                                 .property;
-
-                    label0.Content = "Ready";    
-                };
-
-                backgroundWorker.RunWorkerAsync(topicTextBox.Text);
+                });
 
                 propertyValuesListBoxA.ItemsSource = null;
                 topicPropertiesListBoxB.ItemsSource = null;
@@ -101,14 +105,16 @@ namespace Freebase_WPF
 
                 if (tbl.ContainsKey("id"))
                 {
-                    topicPropertiesListBoxB.ItemsSource =
-                        JsonConvert.DeserializeObject<FreebaseTopic>(
-                            new RestClient("https://www.GoogleAPIs.com")
-                                .Execute(new RestRequest(String.Format("freebase/v1/topic{0}", tbl["id"])))
-                                .Content)
-                        .property;
+                    RequestTopic(tbl["id"] as string, response => 
+                    {
+                        if (response.StatusCode == HttpStatusCode.OK)
+                            topicPropertiesListBoxB.ItemsSource =
+                                JsonConvert
+                                    .DeserializeObject<FreebaseTopic>(response.Content)
+                                    .property;
+                    });
                 }
-                else topicPropertiesListBoxB.ItemsSource = new List<int>();
+                else topicPropertiesListBoxB.ItemsSource = null;
             }
 
             propertyValuesListBoxB.ItemsSource = null;
